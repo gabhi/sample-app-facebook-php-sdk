0dc7edada6b4bcbb281c82f1bc3d026105cf1ba3
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-07-29 23:23:31 -0700

    Modified the app id and secret

--- facebook-php-sample-app/config.php
+++ facebook-php-sample-app/config.php
@@ -5,15 +5,11 @@
  * and open the template in the editor.
  */
 
-require '../src/facebook.php';
 
-$facebook = new Facebook(array(
+$config= array(
   'appId'  => '272382442868360',
   'secret' => 'fb36f3b0c2f2f4c0600b7b853d6c0048',
-));
+);
 
-
- 
-$user = $facebook->getUser();
 
 ?>
