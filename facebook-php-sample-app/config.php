281e6897baed6da4eca4fae0315b6638148f8abf
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-07-29 22:33:02 -0700

    Modified the app id and secret

--- facebook-php-sample-app/config.php
+++ facebook-php-sample-app/config.php
@@ -8,8 +8,8 @@
 require '../src/facebook.php';
 
 $facebook = new Facebook(array(
-  'appId'  => '477422715604345',
-  'secret' => 'b548ebc4ceb9d054562dd457fc3af1ff',
+  'appId'  => '272382442868360',
+  'secret' => 'fb36f3b0c2f2f4c0600b7b853d6c0048',
 ));
 
 
