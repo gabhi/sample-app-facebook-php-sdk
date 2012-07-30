5f99ba3764ba2e10afb171ebe37e7463b9c904fa
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-07-29 22:33:14 -0700

    Modified the app id and secret

--- facebook-php-sample-app/logout.php
+++ facebook-php-sample-app/logout.php
@@ -5,6 +5,8 @@
  * and open the template in the editor.
  */
 include 'config.php';
+include 'constants.php';
+
 setcookie('fbs_'.$facebook->getAppId(), '', time()-100, '/', DOMAIN_NAME);
 session_destroy();
 $home_url = "Location: ".PROJECT_HOME_URL;
