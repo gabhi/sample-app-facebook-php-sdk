43b314b79f4beb2ce231ccd13c4aac0b4e31738b
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-07-30 00:33:48 -0700

    Reformatting

--- facebook-php-sample-app/index.php
+++ facebook-php-sample-app/index.php
@@ -27,36 +27,14 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Bootstrap, from Twitter</title>
+        <title>Sample Facebook App + Twitter bootstrap</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <!-- Le styles -->
-        <link href="../assets/css/bootstrap.css" rel="stylesheet">
-        <style type="text/css">
-            body {
-                padding-top: 60px;
-                padding-bottom: 40px;
-            }
-            .sidebar-nav {
-                padding: 9px 0;
-            }
-        </style>
-        <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-
-        <!-- Le fav and touch icons -->
-        <link rel="shortcut icon" href="../assets/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-    </head>
+        <?php include 'include_style.php';
+        ?>
+           </head>
 
     <body>
 
--- facebook-php-sample-app/top_header.php
+++ facebook-php-sample-app/top_header.php
@@ -82,8 +82,8 @@ if ($user) {
             <div class="nav-collapse">
                 <ul class="nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="http://gabhi.com/">About</a></li>
+                    <li><a href="http://allfollowers.info">Twitter Followers Analytics</a></li>
                 </ul>
             </div><!--/.nav-collapse -->
         </div>
